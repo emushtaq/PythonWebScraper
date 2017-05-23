a7cb9af9edb2469fa5111916e5d42cf8ea9463c1
AkhilSinghRana <akhilsinghrana@gmail.com>
2017-05-23 20:47:38 +0200

    using BeautifulSoup

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-from urllib2 import urlopen
-
+#from urllib2 import urlopen
+import requests
+from BeautifulSoup import BeautifulSoup
+url="https://en.wikipedia.org/wiki/2017_in_film"
 #used to open a web page
-urlopen("https://en.wikipedia.org/wiki/2017_in_film")
+	#urlopen(url)
 
+
+#getting html
+response=requests.get(url)
+html=response.content
+
+soup=BeautifulSoup(html)
+print (soup.prettify())
