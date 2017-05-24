5deb8fdee537917e68c72a6855c7827df68063ff
AkhilSinghRana <akhilsinghrana@gmail.com>
2017-05-24 18:34:20 +0200

    you can read columns from table now

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #from urllib2 import urlopen
 import requests
 from BeautifulSoup import BeautifulSoup
+import csv
 url="https://en.wikipedia.org/wiki/2017_in_film"
 #used to open a web page
 	#urlopen(url)
@@ -11,4 +12,14 @@ response=requests.get(url)
 html=response.content
 
 soup=BeautifulSoup(html)
-print (soup.prettify())
+#print (soup.prettify())
+table=soup.find('table',attrs={'class':'sortable'})
+
+for row in table.findAll('tr'):
+    for column in row.findAll('td'):
+    	print column.text
+
+"""
+outfile=open("./column_data.csv","wb")
+writer=csv.writer()
+"""
