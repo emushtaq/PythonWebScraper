cfc808622edc28167e20fd216b0e8bc5a36bbbc7
emushtaq <eshanmushtaq@gmail.com>
2017-05-25 17:55:49 +0200

    extending fundtionality to scrap multiple keywords

--- insta-scrap.py
+++ insta-scrap.py
@@ -1,4 +1,17 @@
 import subprocess
-hashtag = raw_input("Enter the hashtag that you would like to scrap from Instagram: ")
-number = raw_input("Enter the number of entries you would like to scrap: ")
-subprocess.call(["scraper/bin/instagram-scraper", hashtag, "--tag", "-m", number, "-d", "./data/"+hashtag, "--media_metadata"])
+import csv
+
+#number of images (must be a string)
+number = "10"
+
+keywords = []
+with open('keywords.csv', 'r') as f:
+    for line in f.readlines():
+        keywords.extend(line.split(','))
+
+print "Fetching data for :"
+print keywords
+
+for keyword in keywords:
+	subprocess.call(["scraper/bin/instagram-scraper", keyword, "--tag", "-m", number, "-d", "./data/"+keyword, "--media_metadata"])
+
