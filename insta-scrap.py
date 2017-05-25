70e4f96146765a6a624129e271e941952c003067
emushtaq <eshanmushtaq@gmail.com>
2017-05-25 17:45:35 +0200

    organised data folder

--- insta-scrap.py
+++ insta-scrap.py
@@ -1,4 +1,4 @@
 import subprocess
 hashtag = raw_input("Enter the hashtag that you would like to scrap from Instagram: ")
 number = raw_input("Enter the number of entries you would like to scrap: ")
-subprocess.call(["scraper/bin/instagram-scraper", hashtag, "--tag", "-m", number, "-d", "./data", "--media_metadata"])
+subprocess.call(["scraper/bin/instagram-scraper", hashtag, "--tag", "-m", number, "-d", "./data/"+hashtag, "--media_metadata"])
